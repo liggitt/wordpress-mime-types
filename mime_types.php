10716197c81fd4ff1f7afc16bf8bdaa629e7a2b9
Jordan Liggitt <jordan@liggitt.net>
2013-03-07 16:48:03 -0800

    Initial commit

---
+++ mime_types.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Mime Types
+Plugin URI: https://github.com/liggitt/wordpress-mime-types
+Description: Adds allowed mime-types
+Version: 1.0
+Author: Jordan Liggitt
+Author URI: http://jordan.liggitt.net
+License: GPL2
+*/       
+
+function addUploadMimes($mimes) {
+    $mimes = array_merge($mimes, array(
+        'epub|mobi' => 'application/octet-stream'
+    ));
+    return $mimes;
+}
+add_filter('upload_mimes', 'net_liggitt_add_mime_types');
+
+?>
